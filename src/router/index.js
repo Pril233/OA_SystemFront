3c598e0ee74633964150a2c972858400ad0620c3
Pril233 <414735125@qq.com>
2020-06-18 19:47:13 +0800

    OA_System   login menus depart post commit

--- src/router/index.js
+++ src/router/index.js
@@ -12,12 +12,19 @@ import post from '../components/post/post.vue'
 
 import password from '../components/personal/password.vue'
 import userInfo from '../components/personal/userInfo.vue'
+import todoList from '../components/personal/todoList.vue'
 
 import fileUp from '../components/activity/fileUp.vue'
 import fileDown from '../components/activity/fileDown.vue'
+import addActivity from '../components/activity/addActivity.vue'
+import ViewAct from '../components/activity/viewActivity/ViewActivity.vue'
+import ViewActCommon from '../components/activity/viewActivity/ViewActivityCommon.vue'
+import ViewActVote from '../components/activity/viewActivity/ViewActivityVote.vue'
+import vote from '../components/activity/vote.vue'
 
 import anno from '../components/anno/announcement.vue'
 import annoView from '../components/anno/ViewAnnouncement.vue'
+
 
 
 
@@ -28,11 +35,14 @@ export default new Router({
   routes: [
 
     { path: '/', redirect: '/login' },
+    
 
 
     /*{ path: '/', redirect: '/cardTest' },*/
 
     { path: '/login', component: login },
+    
+   
     {
       path:'/home',
       component:Home,
@@ -46,6 +56,15 @@ export default new Router({
          {path:'/fileDown',component:fileDown},
          {path:'/anno',component:anno},
          {path:'/annos',component:annoView},
+         { path: '/todoList', component: todoList },
+         { path: '/addAct', component: addActivity },
+         {path:'/viewAct',component: ViewAct,
+          children: [
+            { path: '/ViewActivityCommon', component: ViewActCommon },
+            { path: '/ViewActivityVote', component: ViewActVote }
+          ]
+        },
+        { path: '/vote', component: vote}
          
 
       ]
@@ -56,6 +75,9 @@ export default new Router({
 
     // {path:'/childrenText',component:childrenText},
     
-     {path:'/password',component:password}
+     {path:'/password',component:password},
+
+
+    
   ]
 })
